f07422044b2548e3e4005a5adbfd0aa73b622481
Fernando Lopes da silva <fernandolopesti88@gmail.com>
2022-08-06 23:07:40 -0300

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { AloMundoComponent } from './componentes/alo-mundo/alo-mundo.component';
 import { CardTesteComponent } from './componentes/card-teste/card-teste.component';
 import { ListTesteComponent } from './componentes/list-teste/list-teste.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeaderComponent } from './componentes/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AloMundoComponent,
     CardTesteComponent,
-    ListTesteComponent
+    ListTesteComponent,
+    MenuComponent,
+    LogoComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
